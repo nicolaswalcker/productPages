3f09de8a1826e66e70feac72b16c0967382399b2
nicolaswalcker <walcker_@outlook.com>
2021-10-18 00:38:37 -0300

    style: adiciona estilos no header

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const buttonMobile = document.querySelector(".js-mobile");
+
+buttonMobile.addEventListener("click", (e) => {
+  buttonMobile.classList.toggle('u-active')
+
+  /* icon.classList.remove('fas', 'fa-bars') 
+  icon.classList.add('fas', 'fa-times'); */
+});
