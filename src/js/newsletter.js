a550c5d555a8fe88c01b6a90a3f7084d08238de5
nicolaswalcker <walcker_@outlook.com>
2021-10-18 18:28:56 -0300

    refactor/feact: adiciona validacoes na newsletter

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -1,20 +1,17 @@
 const form = document.querySelector(".c-form");
 const inputName = document.querySelector(".js-name");
 const inputMail = document.querySelector(".js-mail");
+const smallName = document.querySelector(".js-small__name");
+const smallEmail = document.querySelector(".js-small__email");
 const urlNews = "https://corebiz-test.herokuapp.com/api/v1/newsletter";
 
 const sendData = () => {
-  console.log("sending data");
   let objJson = {
     email: inputMail.value,
     name: inputName.value,
   };
   let newObj = JSON.stringify(objJson);
   const XHR = new XMLHttpRequest();
-
-  XHR.addEventListener("load", (event) => {
-    alert(event.target.responseText);
-  });
 
   XHR.addEventListener("error", (event) => {
     alert("Oops, algo deu errado");
@@ -23,6 +20,17 @@ const sendData = () => {
   XHR.send(newObj);
 };
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  if (inputName.value === "") {
+    event.preventDefault();
+    smallName.classList.add("u-small__active");
+  } else {
+    smallName.classList.remove("u-small__active");
+  }
+  if (inputMail.value === "") {
+    event.preventDefault();
+    smallEmail.classList.add("u-small__active");
+  } else {
+    smallEmail.classList.remove("u-small__active");
+  }
   sendData();
 });
